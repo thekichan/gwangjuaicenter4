49ecd151f6c57df99d714542b9cc8d8795c7d100
기찬 <133872394+thekichan@users.noreply.github.com>
2023-08-21 13:40:22 +0900

    Add files via upload

---
+++ workspace/ex230821/src/exam/Ex00Intro.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Ex00Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+		// 실행 단축기 : ctrl + f11
+
+	}
+
+}
---
+++ workspace/ex230821/src/exam/Ex03Input.java
@@ -0,0 +1,22 @@
+package exam;
+
+import java.util.Scanner;
+
+public class Ex03Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 입력하기 위한 도구 : Scanner
+		
+		
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.print("나이 입력 : ");
+		int age = sc.nextInt();
+		System.out.println("나이는 : " + age + "살입니다.");
+		
+
+	}
+
+}
